b2cbe54148b52c0426372a6b36ef27c323b7492f
Noraz <Noraz@DESKTOP-J9DQDUS>
2019-01-07 23:03:34 +0100

    ok sprawdz to wszystko, nie wiem czemu srodkowy button mi sie rozjezdzala w main menu sprobuj to naprawic. a tak to chyba wszystko dziala, przejrzyj kod czy jest git.

---
+++ TestOkienka/Class1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestOkienka
+{
+    class PasswordChecker
+
+    {
+       
+        String passConst = "Zupa Pomidorowa";
+        String[] passArray = new String[3]
+               {"kupa" , "gowno" , "sraka"};
+       
+        public void EqualMethod(string passWrittenEM)
+       
+
+        {
+            while (true)
+            {
+
+                if (passWrittenEM.Equals(passConst))
+                {
+                    MessageBox.Show("jestes super odgadles haslo");
+                    break;
+                }
+                else
+                {
+                    MessageBox.Show("jestes Baran spróbuj ponownie:");
+                    break;
+                }
+
+            }
+        }
+
+        public void ArrayMethod(string passWrittenAM)
+            {
+                
+                while (true)
+                {
+                    
+                    if (passArray.Contains(passWrittenAM))
+                    {
+                        MessageBox.Show("Swietnie! Kupa radosci!");
+                        break;
+                    }
+
+                    else
+                    {
+                        MessageBox.Show("Wez sie do kupy chopie!");
+                        break;
+
+                    }
+
+                }
+            }
+
+        public void ContainsMethod(string passWrittenCM)
+
+        {
+                while (true)
+                {
+
+                    if (passWrittenCM.Contains("tajemnica"))
+                    {
+                        MessageBox.Show("Dobra robota");
+                        break;
+                    }
+                    else
+                    {
+                        MessageBox.Show("wpisz ponownie haslo");
+                        break;
+
+                    }
+                }
+
+        }
+
+
+    }
+
+
+}
+
+
+
+
+
